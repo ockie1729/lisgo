c46485597f40b748b300d54727b6b40d33272ba3
mofumofuchan <you_oki1@yahoo.co.jp>
2020-09-01 23:56:20 +0900

    四則演算を実装

--- lis.go
+++ lis.go
@@ -5,6 +5,28 @@ import (
 	"strings"
 )
 
+
+func Eval(expression []interface{}) int {
+	op, _ := expression[0].(string)
+	a, _ := expression[1].(int)
+	b, _ := expression[2].(int)
+
+	var res int
+
+	if op == "add" {
+		res = a + b
+	} else if op == "sub" {
+		res = a - b
+	} else if op == "mul" {
+		res = a * b
+	} else if op == "div" {
+		res = a / b
+	}
+
+	return res
+}
+
+
 func Tokenize(s string) []string {
 	s = strings.Replace(s, "(", " ( ", -1)
 	s = strings.Replace(s, ")", " ) ", -1)
--- lis_test.go
+++ lis_test.go
@@ -12,3 +12,44 @@ func TestTokenize(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestEvalAdd(t *testing.T) {
+	inputExp := []interface{} {"add", 1, 2}
+	got := Eval(inputExp)
+	want := 3
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestEvalSub(t *testing.T) {
+	inputExp := []interface{} {"sub", 1, 3}
+	got := Eval(inputExp)
+	want := -2
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestEvalMul(t *testing.T) {
+	inputExp := []interface{} {"mul", 1, 3}
+	got := Eval(inputExp)
+	want := 3
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestEvalDiv(t *testing.T) {
+	inputExp := []interface{} {"div", 6, 2}
+	got := Eval(inputExp)
+	want := 3
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+}
