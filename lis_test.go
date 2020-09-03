a301859ccf4e2bb5b4aefdcbb9971abdec86a2e2
mofumofuchan <you_oki1@yahoo.co.jp>
2020-09-03 23:25:16 +0900

    使わなくなったimportを削除

--- lis_test.go
+++ lis_test.go
@@ -2,8 +2,6 @@ package main
 
 import "testing"
 
-import "github.com/google/go-cmp/cmp"
-
 func TestTokenize(t *testing.T) {
 	got := Tokenize("(+ 1 2)")
 	want := []string{"(", "+", "1", "2", ")"}
